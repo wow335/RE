e5e7ee23a4c1652fee2447cf169ac50f38ad6d97
kvipka <qvipka@gmail.com>
2015-10-15 18:31:31 +0300

    a little bit clean

--- sql/updates/world/2015_10_14_02_world.sql
+++ sql/updates/world/2015_10_14_02_world.sql
@@ -1,3 +1,4 @@
+--
 DELETE FROM `creature_addon` WHERE `guid` IN (56606,56607);
 INSERT INTO `creature_addon` (`guid`,`path_id`,`mount`,`bytes1`,`bytes2`,`emote`,`auras`) VALUES
 (56606,566050,0,0,1,0, ''),
